89ba0ebcc87ad7e63cec9c235b089d1bc5ab3fa0
Sergio Sutter <sergio.sutter@messerli.ch>
2019-11-20 09:08:58 +0100

    Rename test method

--- zxcvbn-test/ZxcvbnTestMesserliExtended.cs
+++ zxcvbn-test/ZxcvbnTestMesserliExtended.cs
@@ -66,7 +66,7 @@ namespace zxcvbn_test
         [TestMethod]
         [TestCategory(CategoryUnitTest)]
         [Priority(1)]
-        public void SwissGermanKeyboardPatternMatch()
+        public void MatchesSwissGermanKeyboardPattern()
         {
             var swissKeyboardPatterns = new List<string> { "löäü'", "ö-.,m" };
 
@@ -96,7 +96,7 @@ namespace zxcvbn_test
         [TestMethod]
         [TestCategory(CategoryUnitTest)]
         [Priority(1)]
-        public void FrenchKeyboardPatternMatch()
+        public void MatchesFrenchKeyboardPattern()
         {
             var frenchKeyboardPatterns = new List<string> { "*ùmlkj", "bn,;:!" };
 
